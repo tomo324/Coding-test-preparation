714c538c81e929bc212d664969f7d6b7113bc531
tomo324 <0204tomo.mc@gmail.com>
2023-07-04 23:18:00 +0900

    20230704

--- aizu online judge/練習/4章 データ構造/4.3_Queue.py
+++ aizu online judge/練習/4章 データ構造/4.3_Queue.py
@@ -7,37 +7,35 @@ class P:
         self.name = name
         self.t = t
 
-n, q = map(int, input().split())
-
 LEN = 100005
+Q = [P('', 0) for i in range(LEN)]
 
+n, q = map(int, input().split())
 head = 0
 tail = n
-Q = [P('', 0) for _ in range(LEN)]
+
 for i in range(n):
     Q[i].name, num = input().split()
     Q[i].t = int(num)
 
 def enqueue(Q, x, tail):
     Q[tail] = x
-    tail = (tail + 1) % LEN
+    tail = (tail+1) % LEN
     return tail
 
 def dequeue(Q, head):
     x = Q[head]
-    head = (head + 1) % LEN
+    head = (head+1) % LEN
     return x, head
 
-elaps = 0
+elapsed = 0
 
 while head != tail:
     u, head = dequeue(Q, head)
     c = min(q, u.t)
     u.t -= c
-    elaps += c
+    elapsed += c
     if u.t > 0:
         tail = enqueue(Q, u, tail)
     else:
-        print(u.name + " " + str(elaps))
-
-
+        print(u.name + " " + str(elapsed))
