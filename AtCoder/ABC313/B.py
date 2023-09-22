59261e8a5967539ba51d133f9c7d30a411b2f1d1
tomo324 <0204tomo.mc@gmail.com>
2023-09-22 14:24:21 +0900

    Revert "20230829"
    This reverts commit 908ac1cc6798e9ee038f9537db8a547716048100.

--- AtCoder/ABC313/B.py
+++ AtCoder/ABC313/B.py
@@ -1,17 +1,5 @@
 N, M = map(int, input().split())
 
-is_lose = [False] * N
+lst = [list(map(int, input().split())) for _ in range(M)]
 
-for _ in range(M):
-    win, lose = map(int, input().split())
-    is_lose[lose-1] = True
-
-winners = []
-for i in range(N):
-    if not is_lose[i]:
-        winners.append(i+1) #1-indexedに戻す
-
-if len(winners) == 1:
-    print(winners[0])
-else:
-    print(-1)
+for i in range()
