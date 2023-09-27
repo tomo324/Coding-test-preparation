7437b8a7953e8ae069a9f7080009620bca843b2d
tomo324 <0204tomo.mc@gmail.com>
2023-09-27 18:40:56 +0900

    20230927

--- AtCoder/ABC317/c.py
+++ AtCoder/ABC317/c.py
@@ -1,2 +1,36 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+
+def dfs(city, current_len):
+    global max_len
+    max_len = max(max_len, current_len)
+    seen[city] = True
+    # 今の街から伸びる道路をたどって次の街を探す
+    for obj in G[city]:
+        if seen[obj[0]]:
+            continue
+        dfs(obj[0], current_len + obj[1])
+    seen[city] = False
+
 N, M = map(int, input().split())
 
+G = [[] for _ in range(15)]
+
+for i in range(M):
+    A, B, C = map(int, input().split())
+    # 繋がっている街、道の長さの順番で追加
+    G[A].append([B, C])
+    G[B].append([A, C])
+
+max_len = 0
+
+# すべての辺からスタートして試す
+for start in range(1, N+1):
+    # seenを初期化
+    seen = [False for _ in range(15)]
+    if G[start]:
+        # 各スタート地点ごとに行ける最長の道を記録
+        dfs(start, 0)
+
+print(max_len)
--- AtCoder/algorithm/graph.py
+++ AtCoder/algorithm/graph.py
@@ -1,10 +1,42 @@
-h, w = map(int, input().split())
+import sys
+sys.setrecursionlimit(10**6)
 
-field = [[] for _ in range(h)]
+def dfs(h, w):
+    seen[h][w] = True
 
-for i in range(h):
+    for i in range(4):
+        nh = h + dy[i]
+        nw = w + dx[i]
+
+        if nh < 0 or nh >= H or nw < 0 or nw >= W:
+            continue
+        if field[nh][nw] == "#":
+            continue
+        if seen[nh][nw]:
+            continue
+        dfs(nh, nw)
+
+H, W = map(int, input().split())
+field = [[] for _ in range(H) ]
+
+for i in range(H):
     field[i] = input()
 
-mh = {-1, 0, 1, 0}
-mw = {0, 1, 0, 1}
+sh, sw, gh, gw = 0, 0, 0, 0
+for h in range(H):
+    for w in range(W):
+        if field[h][w] == "s":
+            sh, sw = h, w
+        if field[h][w] == "g":
+            gh, gw = h, w
 
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
+
+seen = [[False for _ in range(510)] for _ in range(510)]
+dfs(sh, sw)
+
+if seen[gh][gw]:
+    print("Yes")
+else:
+    print("No")
