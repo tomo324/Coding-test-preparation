6ba03f0e119df511f376e1e5c48ce4d147825838
tomo324 <0204tomo.mc@gmail.com>
2024-08-29 23:43:37 +0900

    20240829

--- AtCoder/ABC300~399/ABC368/B.py
+++ AtCoder/ABC300~399/ABC368/B.py
@@ -1,5 +1,14 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-def count_positive_number(lst):
-    
+count = 0
+
+while True:
+    A.sort(reverse=True)
+    if A[0] == 0 or A[1] == 0:
+        break
+    A[0] -= 1
+    A[1] -= 1
+    count += 1
+
+print(count)
