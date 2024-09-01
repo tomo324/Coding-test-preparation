09d75b0ff6f8b8dfec4d94a334628dea37443c36
tomo324 <0204tomo.mc@gmail.com>
2024-09-01 16:53:40 +0900

    20240901

--- AtCoder/ABC300~399/ABC368/C.py
+++ AtCoder/ABC300~399/ABC368/C.py
@@ -1,3 +1,17 @@
 N = int(input())
 H = list(map(int, input().split()))
 
+T = 0
+
+for h in H:
+    turn = h // 5
+    h -= turn * 5
+    T += turn * 3
+    while h > 0:
+        T += 1
+        if T % 3 == 0:
+            h -= 3
+        else:
+            h -= 1
+
+print(T)
