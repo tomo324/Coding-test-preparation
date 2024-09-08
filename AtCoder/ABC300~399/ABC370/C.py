b37aa04047212173c58564b9eb1c3cbd1ecaef9a
tomo324 <0204tomo.mc@gmail.com>
2024-09-08 17:53:03 +0900

    abc370-c

--- AtCoder/ABC300~399/ABC370/C.py
+++ AtCoder/ABC300~399/ABC370/C.py
@@ -1,21 +1,47 @@
-S = list(input())
-T = list(input())
+# S = list(input())
+# T = list(input())
 
-X = [str(len(S))]
-now = ""
-isSkipped = False
+# X = []
+# now = ""
+# isSkipped = False
 
-for i in range(len(S)):
-    if i == 0 and S[i] < T[i]:
-        isSkipped = True
-        continue
-    if S[i] == T[i]:
-        continue
-    S[i] = T[i]
+# for i in range(len(S)):
+#     if i == 0 and S[i] < T[i]:
+#         isSkipped = True
+#         continue
+#     if S[i] == T[i]:
+#         continue
+#     S[i] = T[i]
+#     X.append(S.copy()) # 参照渡しを防ぐためにcopy()を使う
+# if isSkipped:
+#     S[0] = T[0]
+#     X.append(S.copy())
+
+# print(len(X))
+
+# if len(X) > 0:
+#     for i in range(len(X)):
+#         print("".join(X[i]))
+
+
+S, T = input(), input()
+N = len(S)
+
+X = []
+while S != T:
+    best = ""
+    for i in range(N):
+        if S[i] != T[i]:
+            ns = list(S)
+            ns[i] = T[i]
+            ns = "".join(ns)
+            if best == "":
+                best = ns
+            else:
+                best = min(best, ns)
+    S = best
     X.append(S)
-    print(X)
 
-if isSkipped:
-    S[0] = T[0]
-    X.append(S)
-    print(X)
+print(len(X))
+for x in X:
+    print("".join(x))
