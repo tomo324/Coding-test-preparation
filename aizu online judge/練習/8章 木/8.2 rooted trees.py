ec6367a4c55c900b0cac609adcb59a99db4f2a55
tomo324 <0204tomo.mc@gmail.com>
2024-09-11 23:10:52 +0900

    20240911

---
+++ aizu online judge/練習/8章 木/8.2 rooted trees.py
@@ -0,0 +1,14 @@
+MAX = 100005
+NIL = -1
+
+class Node:
+    def __init__(self):
+        self.p
+        self.l
+        self.r
+
+T = [Node() for _ in range(MAX)]
+
+def print_result(u):
+    print(f'node {u}: parent {T[u].p} : depth {D[u]} : ')
+    
