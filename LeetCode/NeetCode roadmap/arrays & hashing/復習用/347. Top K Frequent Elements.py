f64e4b411d058e0118b8b1dd2425473bcfc17aab
tomo324 <0204tomo.mc@gmail.com>
2024-10-14 23:52:47 +0900

    20241014

---
+++ LeetCode/NeetCode roadmap/arrays & hashing/復習用/347. Top K Frequent Elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)] # O(n*logn)ではなくO(n)にするためsortは使わない
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        for n, c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
