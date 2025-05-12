12bb9da765a7c8a72c0c2175d862c5552298b79e
tomo324 <0204tomo.mc@gmail.com>
2025-05-12 22:42:37 +0900

    20250512

---
+++ AtCoder/practice/ABC214 A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+
+  if (n <= 125) {
+    cout << 4 << endl;
+  } else if (n <= 211) {
+    cout << 6 << endl;
+  } else {
+    cout << 8 << endl;
+  }
+  return 0;
+}
---
+++ AtCoder/practice/ABC231 A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int d;
+    cin >> d;
+
+    cout << d * 0.01 << endl;
+} 
