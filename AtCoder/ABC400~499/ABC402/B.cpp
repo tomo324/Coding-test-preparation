102900f1f5e31ad3a410beeaf852c5785be03d06
tomo324 <0204tomo.mc@gmail.com>
2025-06-04 16:18:22 +0900

    20250604

---
+++ AtCoder/ABC400~499/ABC402/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int q;
+  cin >> q;
+  queue<int> que;
+  for (int i = 0; i < q; i++) {
+    int t, x;
+    cin >> t;
+    if (t == 1) {
+      cin >> x;
+      que.push(x);
+    } else {
+      cout << que.front() << endl;
+      que.pop();
+    }
+  }
+}
