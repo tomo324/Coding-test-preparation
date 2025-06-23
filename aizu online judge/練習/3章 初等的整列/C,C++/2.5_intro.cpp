ddaaeaa6c3c6c79d7ace75876945dd3064f08769
tomo324 <0204tomo.mc@gmail.com>
2025-06-23 14:28:51 +0900

    20250623

---
+++ aizu online judge/練習/3章 初等的整列/C,C++/2.5_intro.cpp
@@ -0,0 +1,26 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_D&lang=jp
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+static const int MAX = 200000;
+
+int main() {
+  int R[MAX], n;
+
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> R[i];
+
+  int maxv = -2e9;
+  int minv = R[0];
+
+  for (int i = 1; i < n; i++) {
+    maxv = max(maxv, R[i] - minv);
+    minv = min(minv, R[i]);
+  };
+
+  cout << maxv << endl;
+
+  return 0;
+}
