295abd9b422eb2a51de94ffd00fafd15a53cd93e
tomo324 <0204tomo.mc@gmail.com>
2025-08-03 16:48:41 +0900

    20250803

---
+++ aizu online judge/練習/3章 初等的整列/C++/bubble_sort.cpp
@@ -0,0 +1,38 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_2_A&lang=jp
+
+#include <iostream>
+using namespace std;
+
+int bubbleSort(int A[], int N) {
+  int sw = 0;
+  bool flag = 1;
+  while (flag) {
+    flag = 0;
+    for (int j = N - 1; j >= 1; j--) {
+      if (A[j] < A[j - 1]) {
+        swap(A[j], A[j - 1]);
+        flag = 1;
+        sw++;
+      }
+    }
+  }
+  return sw;
+}
+
+int main() {
+  int A[100], N, sw;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+  sw = bubbleSort(A, N);
+  
+  for (int i = 0; i < N; i++) {
+    if (i) cout << " ";
+    cout << A[i];
+  }
+  cout << endl;
+  cout << sw << endl;
+
+  return 0;
+} 
