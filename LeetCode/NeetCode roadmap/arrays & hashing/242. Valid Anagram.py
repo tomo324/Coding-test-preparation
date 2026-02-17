d4aada1804e0247974f397891328d6b5d3a1c78a
tomo324 <0204tomo.mc@gmail.com>
2026-02-17 22:56:00 +0900

    20260217

--- LeetCode/NeetCode roadmap/arrays & hashing/242. Valid Anagram.py
+++ LeetCode/NeetCode roadmap/arrays & hashing/242. Valid Anagram.py
@@ -11,3 +11,24 @@ class Solution:
             if countS[c] != countT.get(c, 0):
                 return False
         return True
+    
+
+# #20260217
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+
+#         countS, countT = {}, {}
+
+#         # ちょっと冗長
+#         for c in s:
+#             countS[c] = 1 + countS.get(c, 0)
+#         for c in t:
+#             countT[c] = 1 + countT.get(c, 0)
+        
+#         for s in countS:
+#             if countS.get(s, 0) != countT.get(s, 0):
+#                 return False
+#         return True
