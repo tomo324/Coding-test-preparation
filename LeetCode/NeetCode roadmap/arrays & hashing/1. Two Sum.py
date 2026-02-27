e9fcbe6f8fa6b3d2aeb0d41ef8566919d5bc0729
tomo324 <0204tomo.mc@gmail.com>
2026-02-27 22:03:05 +0900

    20260227

--- LeetCode/NeetCode roadmap/arrays & hashing/1. Two Sum.py
+++ LeetCode/NeetCode roadmap/arrays & hashing/1. Two Sum.py
@@ -7,3 +7,16 @@ class Solution:
                 return [prevMap[diff], i]
             prevMap[n] = i
         return
+    
+
+# # 2026/02/27
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         prevMap = {} # val : index
+#         for i, n in enumerate(nums):
+#             diff = target - n
+#             if diff in prevMap:
+#                 return [prevMap[diff], i]
+#             prevMap[n] = i
+#         return
