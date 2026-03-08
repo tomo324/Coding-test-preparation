6df69572d96cb3cd4319c488775dc5dfc3701b79
tomo324 <0204tomo.mc@gmail.com>
2026-03-08 18:09:42 +0900

    20260308

--- LeetCode/NeetCode roadmap/arrays & hashing/49. Group Anagrams.py
+++ LeetCode/NeetCode roadmap/arrays & hashing/49. Group Anagrams.py
@@ -11,3 +11,16 @@ class Solution:
             ans[tuple(count)].append(s)
 
         return ans.values()
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         group = defaultdict(list)
+
+#         for s in strs:
+#             count = [0] * 26 # a ... z
+#             for c in s:
+#                 count[ord(c) - ord("a")] += 1
+#             group[tuple(count)].append(s)
+        
+#         return list(group.values())
